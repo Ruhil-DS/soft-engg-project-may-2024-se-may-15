d948b164757283bbd2b9a4b16e65bdf9c1a8506d
Archit-Handa <archit20handa@gmail.com>
2024-08-03 01:13:51 +0530

    SCRUM-13 Added api.py

---
+++ LMS/backend/api.py
@@ -0,0 +1,6 @@
+from flask_restful import Resource, Api, reqparse, marshal, fields
+from flask_security import auth_required, roles_accepted, current_user
+from .database import db, User, Role
+
+api = Api(prefix='/api/v1')
+
