70e81636a543162c9aaba3a87bf209df75dc91c5
Archit-Handa <archit20handa@gmail.com>
2024-08-07 01:03:55 +0530

    feature/SCRUM-16 Added frontend for Student Dashboard Landing page
    - Need to integrate the Course Launcher

--- LMS/frontend/src/router/index.js
+++ LMS/frontend/src/router/index.js
@@ -1,13 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Landing from '../views/Landing.vue'
+import Home from '../views/Home.vue'
 import Login from '../components/Login.vue'
 import Signup from '../components/Signup.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    { path: '/', name: 'Home', component: Landing },
-    { path:'/login', name: 'Login', component: Login },
+    { path: '/', name: 'Home', component: Home },
+    { path: '/login', name: 'Login', component: Login },
     { path: '/signup', name: 'Signup', component: Signup },
   ]
 });
