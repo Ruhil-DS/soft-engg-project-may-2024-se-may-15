ba966c2e1ca2e959db390ec1c42b3c43e9c0fd6a
Archit-Handa <archit20handa@gmail.com>
2024-08-14 10:54:37 +0530

    feature/SCRUM-48 Integrated Slide Summarizer with frontend and backend
    - Need to make sure the output is JSON always -> could use Pydantic
    - Need to translate key points for video and slide

--- LMS/backend/gen_ai/slide_summarizer.py
+++ LMS/backend/gen_ai/slide_summarizer.py
@@ -12,7 +12,7 @@ model = ChatGroq(temperature=0.8, model="llama3-8b-8192")
 
 def download_slide(file_id):
     download_url = f'https://drive.google.com/uc?id={file_id}'
-    output = f'../downloads/{file_id}.pdf'
+    output = f'downloads/{file_id}.pdf'
     gdown.download(download_url, output, quiet=False)
     
     print(f"Downloaded file: {output}")
@@ -37,15 +37,18 @@ def get_slide_summary(course, lesson):
         Here is the template for JSON response:
             "summary": <Concise Summary>,
             "key_points": [<Key Point 1>, <Key Point 2>, ...]
+            
+        Do not return an extra text like 'Here is the response' apart from JSON.
         """
     
     prompt = PromptTemplate.from_template(prompt_template)
 
     file_id = lesson.slide_url.split('/d/')[1].split('/')[0]
-    if not os.path.exists(f"../downloads/{file_id}.pdf"):
+    
+    if not os.path.exists(f"downloads/{file_id}.pdf"):
         download_slide(file_id)
     
-    loader = PyPDFLoader(f"../downloads/{file_id}.pdf")
+    loader = PyPDFLoader(f"downloads/{file_id}.pdf")
     docs = loader.load()
 
     chain = load_summarize_chain(model, chain_type="stuff", prompt=prompt)
