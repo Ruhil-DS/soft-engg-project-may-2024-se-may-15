f2d38f9fd305aa2189bf2ed81e8c994a9e7b3aa2
luckyb13 <bafnalaxman13@gmail.com>
2024-08-15 23:43:51 +0530

    SCRUM-57 FIXED RUNNER.SH

--- LMS/runner.sh
+++ LMS/runner.sh
@@ -1,1 +1,10 @@
-# make this file into the main runner executable. 2 threads, one for Vue one for flask.
+#!/bin/bash
+cd frontend
+npm install
+echo "Starting frontend"
+npm run dev &
+cd ../backend
+pip install -r requirements.txt
+cd ../
+echo "Starting backend"
+python3 -m backend.main
