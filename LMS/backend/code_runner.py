172bf9f54b9b71d7f4b631807ec07a9f8de14004
Archit-Handa <archit20handa@gmail.com>
2024-08-18 03:37:09 +0530

    feature/SCRUM-64 Added a code_runner module to run the submitted code

---
+++ LMS/backend/code_runner.py
@@ -0,0 +1,32 @@
+import re
+
+def run_code(code, test_cases):
+    exec(code)
+    
+    match = re.search(r"def\s+(\w+)\s*\(", code)
+    
+    if match:
+        func_name = match.group(1)
+    else:
+        return [{
+            "test_case_id": test_case.test_case_id,
+            "result": "Error: Function not found"
+            } for test_case in test_cases]
+    
+    func = locals()[func_name]
+    
+    results = []
+    for i, test_case in enumerate(test_cases):
+        try:
+            result = func(eval(test_case.input_data))
+            results.append({
+                "test_case_id": test_case.test_case_id,
+                "result": str(result) if result else str(None)
+            })
+        except Exception as e:
+            results.append({
+                "test_case_id": test_case.test_case_id,
+                "result": str(e)
+            })
+
+    return results
