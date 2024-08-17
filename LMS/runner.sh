17f7965afe75c2c36c8cffdd06ca6c1e1ae4efcd
Anirudha Anekal <anirudha.anekal.work@gmail.com>
2024-08-17 11:12:45 +0530

    bug/SCRUM-62 final runner file

--- LMS/runner.sh
+++ LMS/runner.sh
@@ -1,9 +1,20 @@
+#!/bin/bash
+
+# Create and activate the virtual environment and install backend dependencies
+python3 -m venv venv
+source venv/bin/activate
+cd backend
+pip install -r requirements.txt
+cd ..
+
+# Install packages and run frontend
 cd frontend
 npm install
 echo "Starting frontend"
 npm run dev &
-cd ../backend
-pip install -r requirements.txt
 cd ../
+
+# Start backend
+cd backend 
 echo "Starting backend"
-python3 -m backend.main
+python3 main.py
